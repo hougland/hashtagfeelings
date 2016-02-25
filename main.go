f2fe7befe648540ae46f4cac5574d6cd006e3d7b
Ricky Hougland <houglande@gmail.com>
2016-02-25 12:07:01 -0800

    add gitignore file and initial twitter api call without authentication

--- main.go
+++ main.go
@@ -10,17 +10,12 @@ import (
 func main() {
 	fmt.Println("Hello")
 
-	dbinfo := fmt.Sprintf("user=%s dbname=%s sslmode=disable", "BluePenguin", "hashtagfeelings")
-	db, err := sql.Open("postgres", dbinfo)
-	checkErr(err)
-	defer db.Close()
+	// dbinfo := fmt.Sprintf("user=%s dbname=%s sslmode=disable", "BluePenguin", "hashtagfeelings")
+	// db, err := sql.Open("postgres", dbinfo)
+	// checkErr(err)
+	// defer db.Close()
 
-	// temporarily hard coded
-	// InsertHashtag(db, "#capstone")
-
-	ShowAllHashtags(db)
-
-	SelectOneHashtag(db)
+	GetTrends()
 
 }
 
--- twitter.go
+++ twitter.go
@@ -1,7 +1,27 @@
 package main
 
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
 func GetTrends() {
 	// returns array of worldwide trends
+	response, err := http.Get("https://api.twitter.com/1.1/trends/place.json?id=1")
+	if err != nil {
+		fmt.Printf("%s", err)
+		panic(err)
+	} else {
+		defer response.Body.Close()
+		contents, err := ioutil.ReadAll(response.Body)
+		if err != nil {
+			fmt.Printf("%s", err)
+			panic(err)
+		}
+		fmt.Printf("%s\n", string(contents))
+	}
+
 }
 
 func GetTweets() {
