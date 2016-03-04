f2174d898389d6f7084e08fb51f206a0abd2b863
Ricky Hougland <houglande@gmail.com>
2016-03-03 16:41:57 -0800

    switch everything over from main.go to application.go for elastic beanstalk conventions

--- application.go
+++ application.go
@@ -1,5 +1,38 @@
 package main
 
+import (
+	"fmt"
+
+	_ "github.com/lib/pq"
+)
+
 func main() {
 	SetEnvVars() // from local, untracked env.go file which sets secrets
+}
+
+func updateHashtags() {
+	// open db
+	db := OpenDBConnection()
+	defer db.Close()
+
+	// get trends
+	trends := GetTrends()
+
+	// for each trend, make sure it's not in db, get its tweets, run sentiment analysis, save in db
+	for _, trend := range trends {
+		if IsInTable(db, trend) == false {
+			tweets := GetTweets(trend)
+			isSentimental, whichSentiment := SentimentAnalysis(tweets)
+			if isSentimental {
+				InsertHashtag(db, trend.Name, whichSentiment)
+			}
+		}
+	}
+}
+
+func checkErr(err error) {
+	if err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
 }
