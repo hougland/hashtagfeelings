110b0754373af88145084c61f13c42a3dc6f0a70
Ricky Hougland <houglande@gmail.com>
2016-03-06 14:34:25 -0800

    delete more unnecessary things to see if it'll work this time

--- application.go
+++ application.go
@@ -2,46 +2,16 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
-	"os"
 )
 
 func main() {
-	// SetEnvVars() // from local, untracked env.go file which sets secrets
-	// updateHashtags()
-
-	f, _ := os.Create("/var/log/golang/golang-server.log")
-	defer f.Close()
-	log.SetOutput(f)
-
 	http.HandleFunc("/positive", Positive)
 	http.HandleFunc("/negative", Negative)
 
-	log.Printf("Listening on port 5000")
 	http.ListenAndServe(":5000", nil)
 
 }
-
-// func updateHashtags() {
-// 	// open db
-// 	db := OpenDBConnection()
-// 	defer db.Close()
-//
-// 	// get trends
-// 	trends := GetTrends()
-//
-// 	// for each trend, make sure it's not in db, get its tweets, run sentiment analysis, save in db
-// 	for _, trend := range trends {
-// 		if IsInTable(db, trend) == false {
-// 			tweets := GetTweets(trend)
-// 			isSentimental, whichSentiment := SentimentAnalysis(tweets)
-// 			if isSentimental {
-// 				InsertHashtag(db, trend.Name, whichSentiment)
-// 			}
-// 		}
-// 	}
-// }
 
 func checkErr(err error) {
 	if err != nil {
--- database.go
+++ database.go
@@ -4,7 +4,6 @@ import (
 	"database/sql"
 	"os"
 
-	"github.com/ChimeraCoder/anaconda"
 	_ "github.com/lib/pq"
 )
 
@@ -16,37 +15,7 @@ type Hashtag struct {
 
 func OpenDBConnection() *sql.DB {
 	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
-
 	checkErr(err)
 
 	return db
 }
-
-func IsInTable(db *sql.DB, trend anaconda.Trend) bool {
-	var id int
-	err := db.QueryRow("SELECT id FROM hashtags WHERE hashtag = $1", trend.Name).Scan(&id)
-	if err == nil {
-		return true
-	} else if err == sql.ErrNoRows {
-		return false
-	} else {
-		panic(err)
-	}
-}
-
-// func InsertHashtag(db *sql.DB, hashtag string, sentiment string) {
-// 	stmt, err := db.Prepare("INSERT INTO hashtags(hashtag, sentiment) VALUES($1, $2);")
-// 	checkErr(err)
-// 	defer stmt.Close()
-//
-// 	_, err = stmt.Exec(hashtag, sentiment)
-// 	checkErr(err)
-// }
-
-// func SelectRandomHashtag(db *sql.DB, sentiment string) Hashtag {
-// 	var hashtag Hashtag
-// 	err := db.QueryRow("SELECT * FROM hashtags WHERE sentiment = $1 ORDER BY random()", sentiment).Scan(&hashtag.ID, &hashtag.Name, &hashtag.Sentiment)
-// 	checkErr(err)
-//
-// 	return hashtag
-// }
