ee94dea2409596bd02c0fee3e1bbba0a6fc757aa
Ricky Hougland <houglande@gmail.com>
2016-03-09 15:00:54 -0800

    test for opendatabaseifclosed

--- database_test.go
+++ database_test.go
@@ -2,9 +2,23 @@ package main
 
 import (
 	"testing"
+
+	"gopkg.in/DATA-DOG/go-sqlmock.v1"
 )
 
 func TestOpenDBIfClosed(t *testing.T) {
+	var err error
+	db, _, err = sqlmock.New()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer db.Close()
+
+	result := OpenDBIfClosed()
+
+	if result != db {
+		t.Errorf("OpenDBIfClosed() returned a different database")
+	}
 
 }
 
