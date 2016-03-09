77047ba820c19bb4a5fee3594af32fb1013f5ea1
Ricky Hougland <houglande@gmail.com>
2016-03-09 11:53:49 -0800

    add test for getscore func

--- sentiment_test.go
+++ sentiment_test.go
@@ -1,8 +1,6 @@
 package main
 
-import (
-	"testing"
-)
+import "testing"
 
 func TestCreateSentimentQuery(t *testing.T) {
 
@@ -39,5 +37,37 @@ func TestIsSentimental(t *testing.T) {
 }
 
 func TestGetScore(t *testing.T) {
+	var tweet1, tweet2, tweet3 TweetText
+	tweet1.Text = "happy happy happy"
+	tweet1.Polarity = 4
 
+	tweet2.Text = "sad sad sad"
+	tweet2.Polarity = 0
+
+	tweet3.Text = "neutral neutral neutral"
+	tweet3.Polarity = 2
+
+	var sentimentObj1, sentimentObj2, sentimentObj3, sentimentObj4 SentimentQuery
+	sentimentObj1.Data = append(sentimentObj1.Data, tweet1)
+	sentimentObj2.Data = append(sentimentObj2.Data, tweet2)
+	sentimentObj3.Data = append(sentimentObj3.Data, tweet3)
+	sentimentObj4.Data = append(sentimentObj4.Data, tweet1)
+	sentimentObj4.Data = append(sentimentObj4.Data, tweet2)
+
+	cases := []struct {
+		in   SentimentQuery
+		want float64
+	}{
+		{sentimentObj1, 4},
+		{sentimentObj2, 0},
+		{sentimentObj3, 2},
+		{sentimentObj4, 2},
+	}
+
+	for _, c := range cases {
+		score := GetScore(c.in)
+		if score != c.want {
+			t.Errorf("GetScore(%q) == %q, want %q", c, score, c.want)
+		}
+	}
 }
